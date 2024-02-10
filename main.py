4662050c7573c9c910bdc46199f566bfafddf2f1
Sylonius <sylonius9@gmail.com>
2024-02-10 11:34:24 -0700

    Finished "Sum Square Difference", "Find Prime By Index" ("Check If Prime" as helper), "Largest Product In A Series"

--- main.py
+++ main.py
@@ -68,6 +68,8 @@ class MathProblems:
                         largestPalYet = tempString
         return largestPalYet
     def SmallestMultiple(num2, num1 = 1, increment = 1):
+        """Given an integer, and possibly 2 others. this method will find the smallest number that
+        can evenly divide into each number ex: SmallestMultiple(4) would be 12"""
         if(num1 > num2):
             tempNum = num1
             num1 = num2
@@ -85,11 +87,55 @@ class MathProblems:
             else:
                 break
         return n
+    def SumSquareDifference(num1):
+        """Given an integer, this will return (1+2+3 + ... + num)^2 - (1^2 + 2^2 + 3^2 ... + num^2)"""
+        sumOfSquares = 0
+        for i in range(num1+1):
+            sumOfSquares += i**2
+        squareOfSum = 0
+        for i in range(num1+1):
+            squareOfSum += i
+        squareOfSum = squareOfSum**2
+        return squareOfSum-sumOfSquares
+    def FindPrimeByIndex(num1):
+        """given an index of prime, Ex: 2=1, 3=2, 5=3, 7=4, 11=5 etc. this method fill find
+        that index"""
+        currentIndex = -1
+        currentNum = 0
+        while(currentIndex < num1):
+            currentNum += 1
+            if(MathProblems.CheckIfPrime(currentNum)):
+                currentIndex += 1
+        return currentNum
+    def CheckIfPrime(num1):
+        """Helper method to check if num1 is a prime number"""
+        isStillPrime = True
+        for i in range (2, math.ceil((num1 + 2)/2)):
+            if (num1 % i == 0):
+                isStillPrime = False
+                break
+        return isStillPrime
+    def LargestProductInASeries(seriesLength, _largeNum):
+        """given a length of numbers and a huge number (given as a string), this method will find the largest
+        string of numbers that are adjacent with a length that is given, that give the
+        largest number when each character is multipled together"""
+        largestResultSoFar = 0
+        for set in range (len(_largeNum) + 1):
+            currentString = _largeNum[set: set + seriesLength]
+            nums = []
+            for index in range(len(currentString)):
+                nums.append(int((currentString[index:index+1])))
+            tempResult = 1
+            for num in nums:
+                tempResult *= num
+            if(tempResult > largestResultSoFar):
+                largestResultSoFar = tempResult
+        return largestResultSoFar
 
 if __name__ == '__main__':
-    for i in range(1,20):
-        start_time = time.time()
-        print(MathProblems.SmallestMultiple(i), i)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print("Execution time:", round(execution_time,5), "seconds")
+    print(MathProblems.LargestProductInASeries(13,"7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"))
+    #start_time = time.time()
+    #your function here
+    #end_time = time.time()
+    #execution_time = end_time - start_time
+    #print("Execution time:", round(execution_time,5), "seconds")
